f51cd38101deda42cd607c1f19aa8f776c0b9d64
YunusEmreCMD <y.emre-alkan@hotmail.com>
2021-02-16 01:14:34 +0100

    nieuwe root toegevoegd voor movie detail page

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ app.get('/', (req, res) => {
 app.get('/movies', (req,res) => {
     res.send('A list of movies');
 });
+app.get('/movies/:movieId', (req,res) => {
+  res.send(`<h1>Detailpage of movie ${req.params.movieId} </h1>`);
+});
 
 app.listen(3000, () => {
   console.log(`Express web app on localhost:3000`);
