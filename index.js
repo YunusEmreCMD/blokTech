0d5cca26af5e011db4389c99ac23dc8f32999e41
YunusEmreCMD <y.emre-alkan@hotmail.com>
2021-03-01 22:32:29 +0100

    handlebars geinstalleerd, nieuwe routes aangemaakt en dynamische content toegevoegd

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 const app = express();
 const port = 3000
 
+const movies = [
+  {title: "The Hulk", genre: "Action", jaartal: 1980},
+  {title: "Spiderman", genre: "Drama", jaartal: 2012},
+  {title: "Spongebob", genre: "Comedy", jaartal: 2021}
+]
+
 app.use(express.static('static'));
 
-app.set('view engine', 'ejs');
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
-app.get('/ejs', (req,res) => {
+app.get('/handlebars', (req,res) => {
   res.render('home');
 });
-// app.get('/app', (req,res) => {
-//   res.render('pages/links', {
-//     links:items
-//   });
-// });
 app.get('/movies', (req,res) => {
-    res.send('A list of movies');
+    res.render('listOfMovies', {title: "lijst van films", movies});
 });
 app.get('/movies/:movieId', (req,res) => {
   res.send(`<h1>Detailpage of movie ${req.params.movieId} </h1>`);
