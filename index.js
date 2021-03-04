e49388dccb77faf5251477de629c58a09728e89c
YunusEmreCMD <y.emre-alkan@hotmail.com>
2021-03-04 20:21:44 +0100

    .env file aangemaakt, DB geconnect

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const app = express();
-const port = 3000
+const dotenv = require('dotenv').config();
+const port = 3000;
 
-const gebruikers = [
-  {naam: "Yunus Emre Alkan", soortGebruiker: "Werkzoekende", opleidingsniveau: "Hbo", leerjaar: 2, functie: "Creative designer", gewenstDienstverband: "stage"},
-  {naam: "Ali", soortGebruiker: "Werkzoekende", opleidingsniveau: "Universiteit", leerjaar: 3, functie: "Architect", gewenstDienstverband: "stage"},
-  {naam: "Lisa", soortGebruiker: "Werkzoekende", opleidingsniveau: "Mbo", leerjaar: 1, functie: "Apotheek assistente", gewenstDienstverband: "part-time"},
-]
+
+// Array voor dynamische content
+
+// const gebruikers = [
+//   {naam: "Yunus Emre Alkan", soortGebruiker: "Werkzoekende", opleidingsniveau: "Hbo", leerjaar: 2, functie: "Creative designer", dienstverband: "Stage"},
+//   {naam: "Ali", soortGebruiker: "Werkzoekende", opleidingsniveau: "Universiteit", leerjaar: 3, functie: "Architect", dienstverband: "Stage"},
+//   {naam: "Lisa", soortGebruiker: "Werkzoekende", opleidingsniveau: "Mbo", leerjaar: 1, functie: "Apotheek assistente", dienstverband: "Part-time"}
+// ]
+
+// DB Connectie testen
+console.log(process.env.TESTVAR);
 
 app.use(express.static('static'));
 
@@ -21,7 +28,8 @@ app.get('/gebruikers', (req,res) => {
     res.render('lijstMetGebruikers', {title: "Dit zijn alle gebruikers", gebruikers});
 });
 app.get('/gebruikers-kaart', (req,res) => {
-  res.render('lijstMetGebruikersKaart', {title: "Dit zijn alle gebruikers", gebruikers});
+  const gebruiker = gebruikers[0];
+  res.render('lijstMetGebruikersKaart', {title: "Dit zijn alle gebruikers", gebruiker});
 });
 // app.get('/movies/:movieId', (req,res) => {
 //   res.send(`<h1>Detailpage of movie ${req.params.movieId} </h1>`);
