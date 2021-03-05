09f2eaab1c580b6ee0b5eb7eef8348d169d793f0
YunusEmreCMD <y.emre-alkan@hotmail.com>
2021-03-05 12:55:50 +0100

    nieuwe routes aangemaakt en gebruikers.json gemaakt voor content in de DB

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ const port = 3000;
 
 
 // Array voor dynamische content
-const gebruikers = [
-  {naam: "Yunus Emre Alkan", soortGebruiker: "Werkzoekende", opleidingsniveau: "Hbo", leerjaar: 2, functie: "Creative designer", dienstverband: "Stage"},
-  {naam: "Ali", soortGebruiker: "Werkzoekende", opleidingsniveau: "Universiteit", leerjaar: 3, functie: "Architect", dienstverband: "Stage"},
-  {naam: "Lisa", soortGebruiker: "Werkzoekende", opleidingsniveau: "Mbo", leerjaar: 1, functie: "Apotheek assistente", dienstverband: "Part-time"}
-]
+// const gebruikers = [
+//   {naam: "Yunus Emre Alkan", soortGebruiker: "Werkzoekende", opleidingsniveau: "Hbo", leerjaar: 2, functie: "Creative designer", dienstverband: "Stage"},
+//   {naam: "Ali", soortGebruiker: "Werkzoekende", opleidingsniveau: "Universiteit", leerjaar: 3, functie: "Architect", dienstverband: "Stage"},
+//   {naam: "Lisa", soortGebruiker: "Werkzoekende", opleidingsniveau: "Mbo", leerjaar: 1, functie: "Apotheek assistente", dienstverband: "Part-time"}
+// ]
 
 // DB Connectie testen
 // console.log(process.env.TESTVAR);
@@ -43,19 +43,35 @@ app.use(express.static('static'));
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
 app.get('/gebruikers', (req,res) => {
     res.render('lijstMetGebruikers', {title: "Dit zijn alle gebruikers", gebruikers});
 });
-app.get('/gebruikers-kaart', (req,res) => {
+
+app.get('/gebruikers-kaart', async (req,res) => {
+  let gebruikers = {}
   const gebruiker = gebruikers[0];
   res.render('lijstMetGebruikersKaart', {title: "Dit zijn alle gebruikers", gebruiker});
 });
-// app.get('/movies/:movieId', (req,res) => {
-//   res.send(`<h1>Detailpage of movie ${req.params.movieId} </h1>`);
-// });
+
+
+
+app.get('/gebruikersdb', async (req,res) => {
+  let gebruikers = {}
+  gebruikers = await db.collection('gebruikers').find({},{sort: {name: 1}}).toArray();
+  res.render('lijstMetGebruikers', {title: "Dit zijn alle gebruikers", gebruikers});
+});
+
+app.get('/gebruikersdb/:gebruikerId', async (req,res) => {
+  const gebruikers = await db.collection("gebruikers").findOne({ id: req.params.gebruikerId });
+  res.render('gebruikerdetails', {title: "Gebruiker details", gebruikers})
+});
+
+app.get('/movies/:movieId', async (req, res) => {
+  const movie = await db.collection('movies').findOne({ id: req.params.movieId });
+  res.render('moviedetails', {title: "Movie details", movie})
+});
+
+
 app.use(function (req, res, next) {
   res.status(404).send("Sorry can't find that!");
 });
